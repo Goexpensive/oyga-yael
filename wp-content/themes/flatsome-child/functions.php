8508e22943dc613b2f64be387a3e3866bd5ffa78
Fabio Vaccaro <fabio.vaccaro@avature.net>
2016-10-26 17:26:21 -0300

    Restilying MercadoPago

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,17 @@
 <?php
-// Add custom Theme Functions here
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+
+// Our hooked in function - $fields is passed via the filter!
+function custom_override_checkout_fields( $fields ) {
+     unset($fields['billing']['billing_company']);
+     unset($fields['billing']['billing_address_1']);
+     unset($fields['billing']['billing_address_2']);
+     unset($fields['billing']['billing_city']);
+     unset($fields['billing']['billing_postcode']);
+     unset($fields['billing']['billing_country']);
+     unset($fields['billing']['billing_phone']);
+     unset($fields['billing']['billing_state']);
+     unset($fields['order']['order_comments']);
+
+     return $fields;
+}
